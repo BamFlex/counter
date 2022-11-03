a8c6c2c56c2489d0087ac408494f58954c11cbe9
bamidele <bamidelefelix94@gmail.com>
2022-11-03 17:20:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import logo from './logo.svg';
 import './App.css';
+import About from './Components/About';
+import Counter from './Components/Counter';
+import Error from './Components/Error';
+import Home from './Components/Home';
+import NavBar from './Components/NavBar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+    return (
+      <Router>
+        <div className="App">
+          <NavBar/>
+          <Routes>
+            {/* <Link></Link> */}
+            <Route exact path="/" element={<Home/>}/>
+            <Route path="counter" element={<Counter/>}/>
+            <Route path="about" element={<About/>}/>
+            <Route path="*" element={<Error/>}/>
+          </Routes>
+        </div>
+      </Router>
+    );
 }
 
 export default App;
